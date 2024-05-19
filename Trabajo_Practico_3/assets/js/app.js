68ce56d6490d5de36583a8e6c6819c3865436370
MarceloOrtegaDev <ortegamarcelodavid77@gmail.com>
2024-05-19 09:08:04 -0300

    Se leyó las consignas nuevamente y se volvió a plantear la función editarTarea para cumplir con los requisitos, las 4 funciones se encuentran completadas y funcionales, se ajustaron los nombres de las variables para que esté un poco mas relacionado al tema, los mensajes del alert fueron editados para una mejor comprensión y el trabajo procede a ser entregado en tiempo y forma.

--- Trabajo_Practico_3/assets/js/app.js
+++ Trabajo_Practico_3/assets/js/app.js
@@ -1,6 +1,6 @@
 let tareas = [ ]
 let opciones;
-let r = 1;
+let opSalir = 1;
 
 function agregarTarea(nombre) {
     const regex = /^\S+$/;
@@ -8,50 +8,44 @@ function agregarTarea(nombre) {
     tareas.push(nombre)
     alert("Tarea nueva Añadida \n" + nombre)
     } else{
-    alert("Ingrese un valor sin espacios")
+    alert("Ingrese un valor sin espacios!")
     }
 }
 
 function listarTareas() {
     let list = ""
     for (let i = 0; i < tareas.length; i++) {
-        list += tareas[i] + " ";
+        list += tareas[i] + ", ";
     }
     alert(list)
 }
 
 
-function editarTarea(tareas) {
-    const regex = /^\S+$/;
-    let opciones = prompt("Ingrese la posición que quiere editar:")
-    if (regex.test(opciones) && opciones < tareas.length){
-        let nuevaTarea = prompt("Ingrese el nuevo valor de la tarea")
-        if (regex.test(nuevaTarea)){
-            tareas.splice(opciones,1,nuevaTarea)
-            alert("La nueva tarea se llama: "+ nuevaTarea)
+function editarTarea(nombre) {
+    let task = tareas.includes(nombre)
+    if (task == true) {
+        const regex = /^\S+$/;
+        let nuevoNom = prompt("Ingrese un nombre nuevo para la tarea:")
+        if (regex.test(nuevoNom)){
+            tareas.splice(tareas.indexOf(nombre),1,nuevoNom)
+            alert("El nombre nuevo es: "+nuevoNom)
         } else {
-            alert("Ingrese un valor sin espacios")
-        }
-    } else{
-        alert("Ingrese un valor valido")
-    }
-
-}
-
-function eliminarTarea() {
-    let opcioDel = parseInt(prompt("Ingrese el número de la tarea que quiere eliminar: "+ tareas));
-        if (opcioDel.includes(tareas)){
-            tareas.splice(tareas,1, opcioDel)
-                alert("Tarea eliminada")
-            }else{
-                alert("Ingrese un valor correcto")
+            alert("El nombre nuevo no debe contener espacios!")
             }
+        } else {
+            alert("Nombre invalido, intente de nuevo!")
+        }
+}   
+
+function eliminarTarea(nombre) {
+    let task = tareas.includes(nombre)
+    if (task == true) {
+        tareas.splice(tareas.indexOf(nombre), 1)
+    } else {
+        alert("Ese nombre no se encuentra en la lista.")
+    }
 }
-    
-
-
-
-while (r !== 0) {
+while (opSalir !== 0) {
     opciones = parseInt(prompt(`Ingrese la opción a realizar:
     1-Agregar tarea
     2-ListarTareas 
@@ -69,16 +63,16 @@ while (r !== 0) {
             listarTareas(tareas);
             break;
         case 3:
-            editarTarea(tareas);
+            editarTarea(prompt("Ingrese el nombre de la tarea que quiere editar: "+ tareas));
             break;
         case 4:
-            eliminarTarea();
+            eliminarTarea(prompt("Ingrese el nombre del valor que quiere eliminar: "+ tareas));
             break;
         case 0:
-            r = 0;
+            opSalir = 0;
             break;
         default:
-            console.log("ingrese una opcion valida")
+            alert("ingrese una opcion dentro de los parámetros!")
             break;
     }
 }
